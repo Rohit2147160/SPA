c2aebf39c9084bd4bf158b114a5d693f0159c6c7
Rohit2147160 <95644285+Rohit2147160@users.noreply.github.com>
2022-02-09 23:10:46 +0530

    Add files via upload

---
+++ my.js
@@ -0,0 +1,47 @@
+angular.module('myapp',['ngRoute'])
+.config(function($routeProvider)
+{
+    $routeProvider.when('/home',
+    {
+        templateUrl:'home.html',
+        controller:'homecntrl'}).when('/home/:first/:last',
+        {
+            templateUrl:'home.html',
+            controller:'homecntrl'
+        }).when('/about',
+        {
+            templateUrl:'about.html',
+            controller:'aboutcntrl'
+        }).when('/flight',
+        {
+            templateUrl:'flight.html',
+            controller:'flightcntrl'
+        })
+})
+.controller('mycntrl',function()
+{
+
+})
+.controller("homecntrl",function($scope,$routeParams)
+{
+    $scope.message="Hey There"
+    if($routeParams.first&&$routeParams.last){
+        $scope.person={
+            first:$routeParams.first,
+            last:$routeParams.last
+        };
+    }
+
+})
+.controller("aboutcntrl",function($scope)
+{
+    $scope.about=["This is a the about page for the flight website.The owner of the company is Rohit Priyadarshi"];
+})
+.controller("flightcntrl",function($scope,$http)
+{
+    $http.get('flights.json')
+    .success(function(response)
+    {
+        $scope.r_ctrl=response.roaster;
+    });
+});
